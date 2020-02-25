a89d97f4a65011acd9548bc9d6389e1d6330b326
dan-kwiat <dan.kwiat@gmail.com>
2020-02-25 12:35:31 +0000

    refactor(import-cc-beta): extract fetchData method into separate file

--- import-cc-beta/index.js
+++ import-cc-beta/index.js
@@ -1,12 +1,10 @@
 require('dotenv').config()
-const fetch = require('node-fetch')
 const streamBatchPromise = require('stream-batch-promise')
-const cheerio = require('cheerio')
 const getProgressBar = require('./lib/progress')
 const log = require('./lib/logger')
+const fetchData = require('./fetch-data')
 
 const {
-  CC_BETA_URL,
   BATCH_SIZE,
   DB_HOST,
   DB_USER,
@@ -27,48 +25,6 @@ const knex = require('knex')({
 })
 
 const PROGRESS_BAR = getProgressBar('Progress')
-
-const betaUrl = id => `${CC_BETA_URL}?regId=${id}&subId=0`
-
-const scrape = async id => {
-  try {
-    const res = await fetch(betaUrl(id))
-    const html = await res.text()
-    const $ = cheerio.load(html)
-    const activities = $('div.row div.pcg-charity-details__block.col-12 p', '#overview').text() || null
-    const peopleItems = $('div.row div.col-lg-3 ul.pcg-charity-details__stats li.pcg-charity-details__fact', '#people')
-    const peopleCount = $('span.pcg-charity-details__amount--people', peopleItems).toArray().map(x => parseInt($(x).text().trim().replace(/,/g, ''))) || []
-    const peopleTitles = $('span.pcg-charity-details__purpose', peopleItems).toArray().map(x => $(x).text().toLowerCase()) || []
-    const people = peopleTitles.reduce((agg, x, i) => ({ ...agg, [x]: peopleCount[i]}), {})
-    return {
-      id,
-      activities,
-      people,
-    }
-  } catch(e) {
-    log.error(e)
-    return { id: null }
-  }
-}
-
-const timebox = (promise, name, shouldReject=false, ms=5000) => {
-  return new Promise(async (resolve, reject) => {
-    const timeout = setTimeout(() => {
-      const msg = `Promise '${name}' timed out after ${ms}ms`
-      log.error(msg)
-      shouldReject ? reject(new Error(msg)) : resolve(null)
-    }, ms)
-    try {
-      const res = await promise
-      clearTimeout(timeout)
-      resolve(res)
-    }
-    catch(e) {
-      clearTimeout(timeout)
-      reject(e)
-    }
-  })
-}
 
 const update = arr => {
   return new Promise((resolve, reject) => {
@@ -95,8 +51,8 @@ const batchHandler = (items, counter) => {
   PROGRESS_BAR.update(counter)
   return new Promise(async (resolve, reject) => {
     try {
-      const dataArr = await Promise.all(items.map(x => timebox(scrape(x.regno), 'scrape beta')))
-      await update(dataArr.filter(x => x.id))
+      const dataArr = await Promise.all(items.map(x => fetchData(x.regno)))
+      await update(dataArr.filter(x => x))
       resolve()
     } catch(e) {
       reject(e)
@@ -106,7 +62,12 @@ const batchHandler = (items, counter) => {
 
 const f = async () => {
   try {
-    const countQuery = knex(TABLE_MAIN_CHARITY).count('regno', { as: 'numCharities' })
+    const countQuery = knex(TABLE_MAIN_CHARITY)
+      .where({
+        [`${TABLE_MAIN_CHARITY}.activities`]: null,
+        [`${TABLE_MAIN_CHARITY}.people`]: null,
+      })
+      .count('regno', { as: 'numCharities' })
 
     const { numCharities } = (await countQuery)[0]
 
