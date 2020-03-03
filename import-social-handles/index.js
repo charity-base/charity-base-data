523d36598ff316381b3ae903b6dd61f4a5bf4f4c
dan-kwiat <dan.kwiat@gmail.com>
2020-03-03 12:59:46 +0000

    feat(import-social-handles): pool requests so we're not always waiting for the slowest to complete

--- import-social-handles/index.js
+++ import-social-handles/index.js
@@ -6,6 +6,7 @@ const timebox = require('./lib/timebox')
 const { TimeoutError } = require('./lib/errors')
 const fetchData = require('./fetch-data')
 const memwatch = require('@airbnb/node-memwatch')
+const asyncPool = require('tiny-async-pool')
 
 const {
   BATCH_SIZE,
@@ -16,6 +17,7 @@ const {
   OFFSET,
   TABLE_MAIN_CHARITY,
   TABLE_SOCIAL,
+  REQUEST_CONCURRENCY,
 } = process.env
 
 const knex = require('knex')({
@@ -44,33 +46,35 @@ process.on('exit', (code) => {
 })
 
 const update = async arr => {
+  const updateQueries = arr
+    .filter(x => x.url !== x.cleanUrl)
+    .map(({
+      id,
+      url,
+      cleanUrl,
+      twitter,
+      facebook,
+    }) => (
+      knex(TABLE_MAIN_CHARITY)
+        .where('regno', '=', id)
+        .update({ web: cleanUrl })
+    ))
+  const insertQueries = arr
+    .filter(({ twitter, facebook }) => twitter || facebook)
+    .map(({ id, twitter, facebook }) => (
+      knex(TABLE_SOCIAL)
+        .insert({
+          regno: id,
+          twitter,
+          facebook,
+        })
+    ))
+  const allQueries = [...updateQueries, ...insertQueries]
+  if (allQueries.length === 0) {
+    return
+  }
   const transaction = knex.transaction(trx => {
-    const updateQueries = arr
-      .filter(x => x.url !== x.cleanUrl)
-      .map(({
-        id,
-        url,
-        cleanUrl,
-        twitter,
-        facebook,
-      }) => (
-        knex(TABLE_MAIN_CHARITY)
-          .where('regno', '=', id)
-          .update({ web: cleanUrl })
-          .transacting(trx)
-      ))
-    const insertQueries = arr
-      .filter(({ twitter, facebook }) => twitter || facebook)
-      .map(({ id, twitter, facebook }) => (
-        knex(TABLE_SOCIAL)
-          .insert({
-            regno: id,
-            twitter,
-            facebook,
-          })
-          .transacting(trx)
-      ))
-    return Promise.all([...updateQueries, ...insertQueries])
+    return Promise.all(allQueries.map(x => x.transacting(trx)))
       .then(trx.commit)
       .catch(trx.rollback)
   })
@@ -87,11 +91,15 @@ const update = async arr => {
 }
 
 const batchHandler = (items, counter) => {
-  PROGRESS_BAR.update(counter)
   return new Promise(async (resolve, reject) => {
     try {
-      const dataArr = await Promise.all(items.map(({ regno, web }) => fetchData({ id: regno, url: web })))
+      const dataArr = await asyncPool(
+        parseInt(REQUEST_CONCURRENCY),
+        items,
+        fetchData
+      )
       await update(dataArr.filter(x => x))
+      PROGRESS_BAR.update(counter)
       resolve()
     } catch(e) {
       reject(e)
@@ -101,36 +109,39 @@ const batchHandler = (items, counter) => {
 
 const f = async () => {
   try {
+    log.info(`Cleaning url data in '${TABLE_MAIN_CHARITY}' and inserting social handles into '${TABLE_SOCIAL}'`)
+
     const countQuery = knex(TABLE_MAIN_CHARITY)
       .leftJoin(TABLE_SOCIAL, `${TABLE_MAIN_CHARITY}.regno`, '=', `${TABLE_SOCIAL}.regno`)
-      .where({
-        [`${TABLE_SOCIAL}.twitter`]: null,
-        [`${TABLE_SOCIAL}.facebook`]: null,
-      })
+      .whereNull(`${TABLE_SOCIAL}.twitter`)
+      .whereNull(`${TABLE_SOCIAL}.facebook`)
+      .whereNotNull(`${TABLE_MAIN_CHARITY}.web`)
       .count('*', { as: 'numCharities' })
 
     const { numCharities } = (await countQuery)[0]
 
-    PROGRESS_BAR.start(numCharities - OFFSET, 0)
-
     const charitiesToUpdate = knex
-      .select(
-        `${TABLE_MAIN_CHARITY}.regno`,
-        `${TABLE_MAIN_CHARITY}.web`,
-      )
+      .select({
+        id: `${TABLE_MAIN_CHARITY}.regno`,
+        url: `${TABLE_MAIN_CHARITY}.web`,
+      })
       .from(TABLE_MAIN_CHARITY)
       .leftJoin(TABLE_SOCIAL, `${TABLE_MAIN_CHARITY}.regno`, '=', `${TABLE_SOCIAL}.regno`)
       .orderBy(`${TABLE_MAIN_CHARITY}.regno`, 'asc')
       .offset(OFFSET)
-      .where({
-        [`${TABLE_SOCIAL}.twitter`]: null,
-        [`${TABLE_SOCIAL}.facebook`]: null,
-      })
+      .whereNull(`${TABLE_SOCIAL}.twitter`)
+      .whereNull(`${TABLE_SOCIAL}.facebook`)
+      .whereNotNull(`${TABLE_MAIN_CHARITY}.web`)
 
-    log.info(`Cleaning url data in '${TABLE_MAIN_CHARITY}' and inserting social handles into '${TABLE_SOCIAL}'`)
+    const queryStream = charitiesToUpdate.stream()
+    queryStream.on('error', err => {
+      log.error('Query stream error')
+      log.error(err)
+    })
 
+    PROGRESS_BAR.start(numCharities - OFFSET, 0)
     const total = await streamBatchPromise(
-      charitiesToUpdate.stream(),
+      queryStream,
       batchHandler,
       {
         batchSize: BATCH_SIZE,
@@ -138,7 +149,7 @@ const f = async () => {
     )
     PROGRESS_BAR.update(total)
     PROGRESS_BAR.stop()
-    log.info(`Successfully searched for ${total} postcodes`)
+    log.info(`Successfully streamed through ${total} items`)
     await knex.destroy()
   } catch(e) {
     log.error(e)
