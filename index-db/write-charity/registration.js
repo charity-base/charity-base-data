7b8bef335c107696bb42c604a162e32576523a7b
dan-kwiat <dan.kwiat@gmail.com>
2020-04-07 11:17:26 +0100

    feat(index-db): persist registrations json to mysql

--- index-db/write-charity/registration.js
+++ index-db/write-charity/registration.js
@@ -6,28 +6,33 @@ const knex = require('../knex-connection')
 
 const {
   BATCH_SIZE,
-  TABLE_CHARITY,
   TABLE_CHARITY_JSON,
-  TABLE_POSTCODE_GEO,
+  TABLE_MAIN_CHARITY,
+  TABLE_REGISTRATION,
+  TABLE_REMOVE_REF,
 } = process.env
 
 const PROGRESS_BAR = getProgressBar('Progress')
 
 const parser = x => {
-  if (!x.chcId || !x.postcodeGeo) return null
+  if (!x.chcId || !x.registrations) return null
+
+  // Sort by reverse chronological
+  const registrations = x.registrations.sort((a, b) => new Date(b.registrationDate) - new Date(a.registrationDate))
 
   return {
     chcId: x.chcId,
-    postcodeGeo: JSON.stringify(x.postcodeGeo),
+    registrations: JSON.stringify(registrations),
+    lastRegistrationDate: registrations[0].registrationDate,
   }
 }
 
 const update = async arr => {
   const updateQueries = arr
-    .map(({ chcId, postcodeGeo }) => (
+    .map(({ chcId, registrations, lastRegistrationDate }) => (
       knex(TABLE_CHARITY_JSON)
         .where('chcId', '=', chcId)
-        .update({ postcodeGeo })
+        .update({ registrations, lastRegistrationDate })
     ))
   if (updateQueries.length === 0) {
     return
@@ -55,25 +60,32 @@ const batchHandler = (items, counter) => {
 
 const f = async () => {
   try {
-    log.info(`Persisting data from '${TABLE_POSTCODE_GEO}' to '${TABLE_CHARITY_JSON}'`)
+    log.info(`Persisting data from '${TABLE_REGISTRATION}' & '${TABLE_REMOVE_REF}' to '${TABLE_CHARITY_JSON}'`)
 
-    const countQuery = knex(`${TABLE_POSTCODE_GEO} as p`)
-      .count('*', { as: 'numCharities' })
-      .innerJoin(`${TABLE_CHARITY} as c`, 'c.postcode', '=', 'p.id')
-      .where('c.subno', '=', '0')
-      .where('c.orgtype', '=', 'R')
+    const countQuery = knex(`${TABLE_REGISTRATION} as r`)
+      .countDistinct('r.regno as numCharities')
+      .innerJoin(`${TABLE_MAIN_CHARITY} as mc`, 'mc.regno', '=', 'r.regno')
+      .where('r.subno', '=', '0')
 
     const { numCharities } = (await countQuery)[0]
 
     const query = knex
       .select([
-        'c.regno as chcId',
-        'p.postcode_geo as postcodeGeo'
+        `r.regno as chcId`,
+        knex.raw(`JSON_ARRAYAGG(
+          JSON_OBJECT(
+            'registrationDate', r.regdate,
+            'removalDate', r.remdate,
+            'removalCode', TRIM(r.remcode),
+            'removalReason', TRIM(rRef.text)
+          )
+        ) as registrations`),
       ])
-      .from(`${TABLE_POSTCODE_GEO} as p`)
-      .innerJoin(`${TABLE_CHARITY} as c`, 'c.postcode', '=', 'p.id')
-      .where('c.subno', '=', '0')
-      .where('c.orgtype', '=', 'R')
+      .from(`${TABLE_REGISTRATION} as r`)
+      .innerJoin(`${TABLE_MAIN_CHARITY} as mc`, 'mc.regno', '=', 'r.regno')
+      .leftJoin(`${TABLE_REMOVE_REF} as rRef`, 'rRef.code', '=', 'r.remcode')
+      .where('r.subno', '=', '0')
+      .groupBy('r.regno')
 
     const queryStream = query.stream()
     queryStream.on('error', err => {
