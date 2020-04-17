3d1d94f326ecc25780964f8a60f871871a1c0e52
dan-kwiat <dan.kwiat@gmail.com>
2020-04-17 14:39:09 +0100

    refactor(import-cc-soap): extract titleCase method into lib file

--- import-cc-soap/fetch-data.js
+++ import-cc-soap/fetch-data.js
@@ -1,23 +1,9 @@
 const log = require('./lib/logger')
 const soapClient = require('./lib/soap-client')
+const titleCase = require('./lib/title-case')
+const UPPER_TERMS = require('./name-acronyms')
+
 const { CC_SOAP_KEY, REQUEST_TIMEOUT } = process.env
-
-const UPPER_TERMS = ['obe', 'cbe', 'mbe', 'fca', 'frs', 'cllr', 'jp', 'gp', 'dl', 'phd', 'ba', 'ma', 'bsci', 'msci', 'mp', 'ca', 'cfa', 'cpa']
-
-let nameCase = name => {
-  if (!name) return ''
-  const terms = name.trim().toLowerCase().split(/[ /]+/)
-
-  return terms.reduce((agg, x) => {
-    if (UPPER_TERMS.indexOf(x) > -1) {
-      return `${agg}${x.toUpperCase()} `
-    }
-    const barrels = x.split(/[-/]+/)
-    const term = barrels.map(y => `${y.charAt(0).toUpperCase()}${y.slice(1)}`).join('-')
-
-    return `${agg}${term} `
-  }, '').trim()
-}
 
 const fetchData = async id => {
   try {
@@ -35,11 +21,11 @@ const fetchData = async id => {
     }
     const trusteeObjects = result.GetCharityTrusteesResult.Trustee.map(x => ({
       id: x.TrusteeNumber,
-      name: nameCase(x.TrusteeName),
+      name: titleCase(x.TrusteeName, UPPER_TERMS),
       trusteeships: x.RelatedCharitiesCount + 1,
       otherCharities: (x.RelatedCharities || []).map(y => ({
         id: y.CharityNumber,
-        name: nameCase(y.CharityName),
+        name: titleCase(y.CharityName),
         status: y.SubmissionsStatus,
       })),
     }))
